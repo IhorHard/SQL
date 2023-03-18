66b32a71a871077cf11a85f13a5a1ae4ee3ce5f2
IhorHard <IhorHardcorenko@gmail.com>
2023-03-18 20:43:12 +0200

    HW.1

---
+++ DBeaver.HW1/employees.sql
@@ -0,0 +1,79 @@
+INSERT INTO employees (employee_name) VALUES
+	 ('Oliver'),
+	 ('Jack'),
+	 ('Harry'),
+	 ('Jacob'),
+	 ('Charley'),
+	 ('Thomas'),
+	 ('George'),
+	 ('Oskar'),
+	 ('James'),
+	 ('William');
+INSERT INTO employees (employee_name) VALUES
+	 ('Noah'),
+	 ('Liam'),
+	 ('Mason'),
+	 ('Ethan'),
+	 ('Michael'),
+	 ('Alexander'),
+	 ('Daniel'),
+	 ('Ihor'),
+	 ('Maksim'),
+	 ('Edward');
+INSERT INTO employees (employee_name) VALUES
+	 ('Roy'),
+	 ('Egor'),
+	 ('Senya'),
+	 ('Mikki'),
+	 ('Volodya'),
+	 ('Kostya'),
+	 ('Misha'),
+	 ('Dima'),
+	 ('Tanya'),
+	 ('Larisa');
+INSERT INTO employees (employee_name) VALUES
+	 ('Liubov'),
+	 ('Marina'),
+	 ('Ira'),
+	 ('Liza'),
+	 ('Masya'),
+	 ('Petr'),
+	 ('Andrey'),
+	 ('Anton'),
+	 ('Sveta'),
+	 ('George');
+INSERT INTO employees (employee_name) VALUES
+	 ('Nikita'),
+	 ('Vlad'),
+	 ('Marmeladov'),
+	 ('Raskolnikov'),
+	 ('Remark'),
+	 ('Fedor'),
+	 ('Maga'),
+	 ('Ali'),
+	 ('Alik'),
+	 ('Masha');
+INSERT INTO employees (employee_name) VALUES
+	 ('Aleksey'),
+	 ('Ivar'),
+	 ('Valkirya'),
+	 ('Odin'),
+	 ('Ragnar'),
+	 ('Rolo'),
+	 ('Ursa'),
+	 ('Hard'),
+	 ('Guts'),
+	 ('Wex');
+INSERT INTO employees (employee_name) VALUES
+	 ('Vadim'),
+	 ('Pasha'),
+	 ('Egor Letov'),
+	 ('Marta'),
+	 ('Noob'),
+	 ('Skorpion'),
+	 ('Sasha'),
+	 ('Maks'),
+	 ('Vova'),
+	 ('Mircha');
+INSERT INTO employees (employee_name) VALUES
+	 ('Marusya');
