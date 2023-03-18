cc9f8d46233c44a2c565a85d915e1e26df32375d
IhorHard <IhorHardcorenko@gmail.com>
2023-03-18 15:06:09 +0200

    trening

---
+++ DBeaver/roles_202303181459.sql
@@ -0,0 +1,8 @@
+INSERT INTO roles (role_title) VALUES
+	 ('QA_manual_junior'),
+	 ('QA_manual_middle'),
+	 ('QA_manual_senior'),
+	 ('QA_Developer_junior'),
+	 ('QA_Developer_middle'),
+	 ('QA_Developer_senior'),
+	 ('Manager');
