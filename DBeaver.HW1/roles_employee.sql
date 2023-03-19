39aedc29e89e96bb99da22fa4f01c811b343ea7b
IhorHard <IhorHardcorenko@gmail.com>
2023-03-19 17:15:00 +0200

    HW.1

---
+++ DBeaver.HW1/roles_employee.sql
@@ -0,0 +1,44 @@
+INSERT INTO roles_employee (employee_id,role_id) VALUES
+	 (1,2),
+	 (2,1),
+	 (3,2),
+	 (4,4),
+	 (5,8),
+	 (6,10),
+	 (7,11),
+	 (8,12),
+	 (9,12),
+	 (10,14);
+INSERT INTO roles_employee (employee_id,role_id) VALUES
+	 (11,2),
+	 (12,3),
+	 (13,4),
+	 (14,1),
+	 (15,8),
+	 (16,14),
+	 (17,19),
+	 (18,15),
+	 (19,16),
+	 (20,12);
+INSERT INTO roles_employee (employee_id,role_id) VALUES
+	 (21,19),
+	 (22,17),
+	 (23,10),
+	 (24,18),
+	 (25,1),
+	 (26,9),
+	 (27,12),
+	 (28,20),
+	 (29,16),
+	 (30,5);
+INSERT INTO roles_employee (employee_id,role_id) VALUES
+	 (31,5),
+	 (32,11),
+	 (33,14),
+	 (34,8),
+	 (35,5),
+	 (36,2),
+	 (37,5),
+	 (38,15),
+	 (39,17),
+	 (40,11);
