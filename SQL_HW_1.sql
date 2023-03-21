a8e1f44bc07a45dbeb054b9a4d3c7bfde5154d61
IhorHardcorenko <124258993+IhorHard@users.noreply.github.com>
2023-03-21 15:40:18 +0200

    SQL_HW_1.sql

--- SQL_HW_1.sql
+++ SQL_HW_1.sql
@@ -3,13 +3,29 @@ select * from students
 ------------------------------
 #Вывести всех студентов в таблице 
 select name
-from students
+   from students
 ------------------------------
 #Выыести только ID пользователей 
 select id
-from students
+   from students
 ------------------------------
 #Вывести только имя пользователей 
 select name 
-from students 
+   from students 
+------------------------------
+#Вывести только email пользователей 
+select  email 
+   from  students 
+------------------------------
+#Вывести только имя и email пользователей 
+select  email,name
+   from  students 
+------------------------------
+#Вывести только id имя email и дату создания  пользователей 
+select  email,name,id,created_on
+   from  students 
+------------------------------
+#Вывести пользователей пользователей где password '12333'
+select * from students 
+  where password like '12333'
 ------------------------------
